1dd4de04ff7423187c0317713644bd9c303454d5
Anishwil8 <anish.wilson@gmail.com>
2021-11-16 22:13:35 -0500

    Assignmnets 1 and 2

---
+++ SeleniumPracrtice/src/Practice/Mukesh_Ass1.java
@@ -0,0 +1,30 @@
+package Practice;
+
+public class Mukesh_Ass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char a='b';
+		char b=85;
+		System.out.println(a+b);
+
+		byte d=100;
+		short e=20;
+		byte f=(byte) (d+e);
+		System.out.println(f);
+
+
+		int g=100;
+		long h=20;
+		int i=(int) (g+h);
+		System.out.println(i);
+
+		float x= (float) 5.0;
+		double y=15.2;
+		double z=(double) (x+y);
+		System.out.println(z);
+		
+	}
+
+}
